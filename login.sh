33e64daff22add91ee8707176fe98d89aca344ac
Matthew Bahr <matt.bahr@gmail.com>
2020-04-04 15:08:39 -0400

    Updated

--- login.sh
+++ login.sh
@@ -31,7 +31,7 @@ echo "##############"
 #Login, save header and cookies, trim header down to not have excess white space
 wget -O login_response -q --server-response --save-cookies cookies --keep-session-cookies --post-data="userName=$username&password=$password" "https://$website/formslogin/Login" 2>&1 | grep \ RequestVerificationToken | cut -c 3- > header
 
-rm Login -f
+rm login_response -f
 
 
 [ -z $1 ] || ./passless.sh $1
